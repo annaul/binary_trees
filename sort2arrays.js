bce662251dc7d18785822868a89642ebdbe3e1bd
Anna Ulanova <anetteua0@gmail.com>
2017-04-16 00:49:12 -0700

    added arr3 asignment and concat

--- sort2arrays.js
+++ sort2arrays.js
@@ -6,7 +6,7 @@ var arr3 = [];
 
 function merge(arr1, arr2) {
   while(arr1 || arr2) {
-    if ( arr1.length === 0 || arr2.length === 0) {arr3.push(arr1, arr2); break;}
+    if ( arr1.length === 0 || arr2.length === 0) {arr3 = arr3.concat(arr1, arr2); break;}
     if (arr1[0] < arr2[0]) {arr3.push(arr1[0]); arr1.shift();}
     if (arr1[0] > arr2[0]) {arr3.push(arr2[0]); arr2.shift();}
   }
